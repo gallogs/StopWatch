0a9266edbf91e0ff64e1142a3352c8c5696f3599
gallogs <gallomatheus.ti@gmail.com>
2024-02-13 16:57:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bem vindo ao StopWatch!!!");
+        Console.WriteLine("");
+        Console.WriteLine("Por favor,  digite o tempo que deseja cronometrar: ");
+        int time = int.Parse(Console.ReadLine());
+
+        Start(time);
+
+
+
+
+
+    }
+
+
+    static void Start(int time)
+    {
+        //int time = 10;
+        int currentTime = 0;
+
+
+        while (currentTime != time)
+        {
+            //Limpando para mostrar apenas o atual
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            //Thread Sleep para ele "dormir" por um certo tempo
+            Thread.Sleep(1000);
+
+            if(currentTime == time)
+            {
+                Console.WriteLine("SEU TEMPO ACABOU!!!!");
+            }
+
+        }
+
+
+    }
+
+
+
+
+
+
+}
